4125dfd9a29bec9dd155de335dee8d33a73fa80f
jimbucktoo <jamesliang.g@gmail.com>
2018-09-13 15:10:31 -0600

    initialization.

---
+++ public/index.js
@@ -0,0 +1,13 @@
+var event = {year: 2018, 
+            month: 'September', 
+            day: 13, 
+            title: "Title",
+            description: "Description", 
+            address: "52 Wallaby Way"};
+
+var post = document.getElementById("post");
+
+post.innerHTML = event.address;
+
+
+
